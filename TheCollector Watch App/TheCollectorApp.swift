0d4c4ecedf9422dd3d3cc78bed1941d02372d9d2
huyvu8051 <huyvu8051@gmail.com>
2024-06-07 23:46:10 +0700

    Initial Commit

---
+++ TheCollector Watch App/TheCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheCollectorApp.swift
+//  TheCollector Watch App
+//
+//  Created by huy on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TheCollector_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheCollector/TheCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheCollectorApp.swift
+//  TheCollector
+//
+//  Created by huy on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TheCollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
