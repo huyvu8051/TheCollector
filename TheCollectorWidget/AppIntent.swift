391e6b2fd8b2d96e6317f07902a13c631357e40e
huyvu8051 <huyvu8051@gmail.com>
2024-06-08 16:34:40 +0700

    add widget module

---
+++ TheCollectorWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  TheCollectorWidget
+//
+//  Created by huy on 6/8/24.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
